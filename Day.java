0bba46662a9d1ac93f66794ff2364b26a9286706
Blitz00X <143121150+Blitz00X@users.noreply.github.com>
2025-06-04 23:47:41 +0300

    Implement simple daily spending tracker

--- Day.java
+++ Day.java
@@ -1,20 +1,53 @@
-public class Day{
+public class Day {
     private String date;
-    private double breakfast;
-    private double travelmoney;
-    private double lunch;
-    private double dinner;
-    private double teeandcoffee;
-    private double other;
-    
-    //default constructor
-    public Day(){
-        
+    private double categoryA;
+    private double categoryB;
+    private double categoryC;
+    private double categoryD;
+    private double categoryE;
+    private double sum;
+    private double left;
 
+    public Day(String date, double a, double b, double c, double d, double e, double total) {
+        this.date = date;
+        this.categoryA = a;
+        this.categoryB = b;
+        this.categoryC = c;
+        this.categoryD = d;
+        this.categoryE = e;
+        this.sum = a + b + c + d + e;
+        this.left = total - this.sum;
     }
 
-    public Day(String date,double breakfast,double travelmoney,double lunch,double dinner,double teeandcofee,double other){
-        
+    public String getDate() {
+        return date;
     }
-    
+
+    public double getCategoryA() {
+        return categoryA;
+    }
+
+    public double getCategoryB() {
+        return categoryB;
+    }
+
+    public double getCategoryC() {
+        return categoryC;
+    }
+
+    public double getCategoryD() {
+        return categoryD;
+    }
+
+    public double getCategoryE() {
+        return categoryE;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public double getLeft() {
+        return left;
+    }
 }
